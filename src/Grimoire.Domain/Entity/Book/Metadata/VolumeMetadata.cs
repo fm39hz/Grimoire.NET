89be1e4029680e352770c0e3383c56a0bac55559
fm39hz <hitpoint2k3@gmail.com>
2025-12-03 18:15:49 +0700

    chore(metadata): add Volume cover

--- src/Grimoire.Domain/Entity/Book/Metadata/VolumeMetadata.cs
+++ src/Grimoire.Domain/Entity/Book/Metadata/VolumeMetadata.cs
@@ -5,6 +5,11 @@ namespace Grimoire.Domain.Entity.Book.Metadata;
 /// </summary>
 public sealed record VolumeMetadata : BaseModel {
 	/// <summary>
+	///     Gets or sets the URL of the cover image for the volume
+	/// </summary>
+	public string? CoverImageUrl { get; init; } = string.Empty;
+
+	/// <summary>
 	///     Gets or sets the publication date of the volume.
 	/// </summary>
 	public DateTime? PublicationDate { get; init; }
