4f52ec4f1d5ed5a7020aa965083bfae48d4fd596
fm39hz <hitpoint2k3@gmail.com>
2025-12-04 00:00:20 +0700

    style(constructor): use primary constructor

--- src/Grimoire.Application/Dto/Book/ChapterVariantResponseDto.cs
+++ src/Grimoire.Application/Dto/Book/ChapterVariantResponseDto.cs
@@ -6,24 +6,13 @@ using Domain.Entity.Book.Segment;
 using System;
 using System.Collections.Generic;
 
-public class ChapterVariantResponseDto : IResponseDto {
-    public Guid Id { get; init; }
-    public Guid ChapterId { get; init; }
-    public VariantType Type { get; init; }
-    public string Language { get; init; }
-    public string? SourceName { get; init; }
-    public int WordCount { get; init; }
-    public List<SegmentModel> Content { get; init; }
-    public List<FootnoteSegmentModel> Footnotes { get; init; }
-
-    public ChapterVariantResponseDto(ChapterVariantModel variant) {
-        Id = variant.Id;
-        ChapterId = variant.ChapterId;
-        Type = variant.Type;
-        Language = variant.Language;
-        SourceName = variant.SourceName;
-        WordCount = variant.WordCount;
-        Content = variant.Content;
-        Footnotes = variant.Footnotes;
-    }
+public class ChapterVariantResponseDto(ChapterVariantModel variant) : IResponseDto {
+	public Guid Id { get; init; } = variant.Id;
+	public Guid ChapterId { get; init; } = variant.ChapterId;
+	public VariantType Type { get; init; } = variant.Type;
+	public string Language { get; init; } = variant.Language;
+	public string? SourceName { get; init; } = variant.SourceName;
+	public int WordCount { get; init; } = variant.WordCount;
+	public List<SegmentModel> Content { get; init; } = variant.Content;
+	public List<FootnoteSegmentModel> Footnotes { get; init; } = variant.Footnotes;
 }
