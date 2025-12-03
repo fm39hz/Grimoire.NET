cfc2f9293842d411ef07dd9d6fd7056e4568b906
fm39hz <hitpoint2k3@gmail.com>
2025-12-03 17:23:49 +0700

    fix(warning): made VolumeMetadata nullable

--- src/Grimoire.Application/Dto/Book/VolumeResponseDto.cs
+++ src/Grimoire.Application/Dto/Book/VolumeResponseDto.cs
@@ -8,6 +8,6 @@ public class VolumeResponseDto(VolumeModel volume) : IResponseDto {
 	public Guid SeriesId { get; init; } = volume.SeriesId;
 	public int Order { get; init; } = volume.Order;
 	public string Title { get; init; } = volume.Title;
-	public VolumeMetadata Metadata { get; init; } = volume.Metadata;
+	public VolumeMetadata? Metadata { get; init; } = volume.Metadata;
 	public Guid Id { get; init; } = volume.Id;
 }
