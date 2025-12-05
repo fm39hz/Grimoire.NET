11d52abffac21782f1ad75b22466b39e98457fe4
fm39hz <hitpoint2k3@gmail.com>
2025-12-06 00:49:10 +0700

    refactor(dto): rename CoverImageUrl to CoverImage
    - Simplify property name in VolumeMetadataDto
    - More concise naming convention

--- src/Grimoire.Application/Dto/Book/Metadata/VolumeMetadataDto.cs
+++ src/Grimoire.Application/Dto/Book/Metadata/VolumeMetadataDto.cs
@@ -4,7 +4,7 @@ public class VolumeMetadataDto {
 	/// <summary>
 	///     Gets or sets the URL of the cover image for the volume
 	/// </summary>
-	public string? CoverImageUrl { get; init; }
+	public string? CoverImage { get; init; }
 
 	/// <summary>
 	///     Gets or sets the publication date of the volume.
