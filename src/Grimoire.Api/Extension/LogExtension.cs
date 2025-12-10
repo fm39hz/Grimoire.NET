db532b14ecb9515cde1f3f8c200114d3e785d581
fm39hz <hitpoint2k3@gmail.com>
2025-12-10 11:04:24 +0700

    style(log): enrich log context

--- src/Grimoire.Api/Extension/LogExtension.cs
+++ src/Grimoire.Api/Extension/LogExtension.cs
@@ -3,20 +3,26 @@ namespace Grimoire.Api.Extension;
 using JetBrains.Annotations;
 using Serilog;
 using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
 
 public static class LogExtension {
 	[UsedImplicitly]
 	public static IServiceCollection AddLog(this IServiceCollection services, WebApplicationBuilder builder) {
+		const string template =
+			"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception} {Properties:j}";
 		Log.Logger = new LoggerConfiguration()
-			.WriteTo.Console()
-			.CreateBootstrapLogger();
+			.WriteTo.Console(outputTemplate : template).CreateBootstrapLogger();
 
 		builder.Host.UseSerilog((context, provider, configuration) => configuration
 			.ReadFrom.Configuration(context.Configuration)
 			.ReadFrom.Services(provider)
 			.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
 			.MinimumLevel.Override("System", LogEventLevel.Warning)
-			.Enrich.FromLogContext().WriteTo.Console());
+			.Enrich.FromLogContext()
+			.Enrich.WithExceptionDetails()
+			.WriteTo.Async(a => a.Console(
+				outputTemplate : template, theme : SystemConsoleTheme.Colored)));
 		return services;
 	}
 }
