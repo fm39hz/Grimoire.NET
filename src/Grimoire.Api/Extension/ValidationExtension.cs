88c9679e5e2cf5cb12f5ac61a329dfa6aac882cf
fm39hz <hitpoint2k3@gmail.com>
2026-06-13 14:39:09 +0700

    refactor(db): unify model validation error responses with global exception middleware

--- src/Grimoire.Api/Extension/ValidationExtension.cs
+++ src/Grimoire.Api/Extension/ValidationExtension.cs
@@ -1,9 +1,13 @@
 namespace Grimoire.Api.Extension;
 
+using System.Diagnostics;
+using System.Linq;
 using Application.Dto.Book.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 public static class ValidationExtension {
 	[UsedImplicitly]
@@ -11,6 +15,31 @@ public static class ValidationExtension {
 		services.AddFluentValidationAutoValidation(config => config.DisableDataAnnotationsValidation = true);
 
 		services.AddValidatorsFromAssemblyContaining<CreateSeriesRequestDtoValidator>();
+
+		services.Configure<ApiBehaviorOptions>(options => {
+			options.InvalidModelStateResponseFactory = context => {
+				var errors = context.ModelState
+					.Where(e => e.Value?.Errors.Count > 0)
+					.ToDictionary(
+						kvp => kvp.Key,
+						kvp => kvp.Value!.Errors.Select(x => x.ErrorMessage).ToArray()
+					);
+
+				var problemDetails = new ProblemDetails {
+					Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+					Title = "One or more validation errors occurred.",
+					Status = StatusCodes.Status400BadRequest,
+					Extensions = {
+						["errors"] = errors,
+						["traceId"] = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier
+					}
+				};
+
+				return new BadRequestObjectResult(problemDetails) {
+					ContentTypes = { "application/problem+json" }
+				};
+			};
+		});
 
 		return services;
 	}
